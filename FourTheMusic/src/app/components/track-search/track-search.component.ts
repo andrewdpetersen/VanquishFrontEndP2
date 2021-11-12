ed32d56c25fdfd464ef3865acab0577b30d7134b
andrewdpetersen <andrew.petersen@revature.net>
2021-11-12 12:00:05 -0600

    search div fully working and tested

--- FourTheMusic/src/app/components/track-search/track-search.component.ts
+++ FourTheMusic/src/app/components/track-search/track-search.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Artist } from 'src/app/interfaces/artist';
 import { Track } from 'src/app/interfaces/track';
+import { Album } from 'src/app/interfaces/album';
+import { AlbumService } from 'src/app/services/album.service';
+import { ArtistService } from 'src/app/services/artist.service';
 import { TrackService } from 'src/app/services/track.service';
 
 @Component({
@@ -9,17 +13,23 @@ import { TrackService } from 'src/app/services/track.service';
   styleUrls: ['./track-search.component.css']
 })
 export class TrackSearchComponent implements OnInit {
-  title:String='';
+  search:String='';
+  searchType:String='';
   trackListResults: Track[]=[];
+  artistListResults: Artist[]=[];
+  albumListResults: Album[]=[];
 
-  constructor(private service:TrackService) { }
+  constructor(private service:TrackService,
+    private service2:ArtistService,
+    private service3:AlbumService) { }
 
-  searchTracks(title:String):Observable<Track[]>{
-    return this.service.SearchTracks(title);
+  searchTracks(search:String):Observable<Track[]>{
+    return this.service.SearchTracks(search);
   }
 
-  listTracks(title:String):void{
-    this.service.SearchTracks(title).subscribe(data=>{
+  listSearch(search:String,searchType:String):void{
+    if(searchType=='track'){
+      this.service.SearchTracks(search).subscribe(data=>{
       for(const single of data){
         let {track_id, title, artist, album} = single;
         this.trackListResults.push({track_id, title, artist, album})
@@ -27,6 +37,24 @@ export class TrackSearchComponent implements OnInit {
       }
     }
       )
+    }else if(searchType=='artist'){
+      this.service2.SearchArtists(search).subscribe(data=>{
+        for(const artist of data){
+          let {ID, name, image_url} = artist;
+          this.artistListResults.push({ID, name, image_url});
+          console.log(artist);
+        }
+      })
+    }else if(searchType=='album'){
+      this.service3.SearchAlbums(search).subscribe(data=>{
+        for(const album of data){
+          let {ID, albumTitle, date, artist, genre} = album;
+          this.albumListResults.push({ID, albumTitle, date, artist, genre});
+          console.log(album);
+    }
+    
+  })
+}
   }
 
   ngOnInit(): void {
